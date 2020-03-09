00e0e63558a9556ed849379e130ede2d9addd491
aayush-aryan <log2aayusharyan@gmail.com>
2020-03-09 12:50:04 +0530

    [aayush]added. Part time Feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,14 @@
 echo " ****** WELCOME TO EMPLOYEE WAGE COMPUTATION ***** "
 WAGE_PER_HRS=20
 IS_FULL_TIME=1
-empCheck=$((RANDOM%2))
+IS_PART_TIME=2
+empCheck=$((RANDOM%3))
 if [ $IS_FULL_TIME -eq $empCheck ]
 then
 		empHrs=8;
+elif [ $IS_PART_TIME -eq $empCheck ]
+then
+			empHrs=4;
 else
 		empHrs=0;
 fi
