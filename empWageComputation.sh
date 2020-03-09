4ef95a29b26d49d8f431373b3dc3cb1e140bf45b
aayush-aryan <log2aayusharyan@gmail.com>
2020-03-09 13:00:52 +0530

    [aayush]rectified. if else statement with case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,14 +4,16 @@ WAGE_PER_HRS=20
 IS_FULL_TIME=1
 IS_PART_TIME=2
 empCheck=$((RANDOM%3))
-if [ $IS_FULL_TIME -eq $empCheck ]
-then
-		empHrs=8;
-elif [ $IS_PART_TIME -eq $empCheck ]
-then
-			empHrs=4;
-else
-		empHrs=0;
-fi
+case $empCheck in
+	$IS_FULL_TIME)
+                  empHrs=8;
+                  ;;
+	$IS_PART_TIME)
+                  empHrs=4;
+                  ;;
+    				*)
+					   empHrs=0;
+                  ;;
+esac
 dailyWage=$(( $empHrs*$WAGE_PER_HRS ))
 echo $dailyWage
