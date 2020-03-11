a9d27b56b9bde2d01c5fd846cfcc1d1bb4973712
aayush-aryan <log2aayusharyan@gmail.com>
2020-03-11 10:13:13 +0530

    [aayush]Refactor. getting work hour using finction

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,21 +7,24 @@ IS_FULL_TIME=1;
 IS_PART_TIME=2;
 totalWorkingDays=0;
 totalEmployeeHrs=0;
+function getWorkHrs(){
+   empCheck=$1
+   case $empCheck in
+         $IS_FULL_TIME)  empHrs=8;
+                         ;;
+         $IS_PART_TIME)  empHrs=4;
+                         ;;
+                     *)
+                         empHrs=0
+                         ;;
+   esac
+   echo $empHrs
+}
 while (( totalEmployeeHrs<=$TOTAL_WORKING_HRS && totalWorkingDays<=$WORKING_DAY_PER_MONTH ))
 do
 	((totalWorkingDays++));
 	empCheck=$((RANDOM%3));
-	case $empCheck in
-		$IS_FULL_TIME)
-      	            empHrs=8;
-         	         ;;
-		$IS_PART_TIME)
-            	      empHrs=4;
-               	   ;;
-    					*)
-					   	empHrs=0;
-                  	;;
-	esac
+	empHrs="$( getWorkHrs $empCheck )";
    totalEmployeeHrs=$(( $totalEmployeeHrs + $empHrs ));
    dailyWage=$(( $empHrs * $WAGE_PER_HRS ));
    echo $dailyWage;
